bdbd57960bd0a23e22af93dfae1e11577ff666f2
misa998 <milosilic998@gmail.com>
2021-11-28 16:10:03 +0100

    Added html template with modules

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
-  styleUrls: ['./recipes-list.component.css']
+  styleUrls: ['./recipes-list.component.css'],
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'description',
+      'https://cdn.pixabay.com/photo/2015/04/29/19/33/cookbook-746005_1280.jpg'
+    ),
+    new Recipe(
+      'A test recipe',
+      'description',
+      'https://cdn.pixabay.com/photo/2015/04/29/19/33/cookbook-746005_1280.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
